d08af8c5ca75d2c0a9a3a6e771e5a020b3151d5d
Thomas Le Bihan <thomas.66days@gmail.com>
2021-05-10 09:46:31 +0200

    add container on header box

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,15 +5,19 @@ const Header = () => {
   return (
     <div className="header-bg">
       <img src={tearSvg} alt="" className="tear-svg" />
-      <div className="header-box">
-        <div className="header-box-content">
-          <span className="header-box-content-tagline">
-            Prêts à faire du tri dans vos placards ?
-          </span>
-          <button className="header-box-content-cta">Commencer à vendre</button>
-          <span className="header-box-content-about">
-            Découvrir comment ça marche
-          </span>
+      <div className="header-container">
+        <div className="header-box">
+          <div className="header-box-content">
+            <span className="header-box-content-tagline">
+              Prêts à faire du tri dans vos placards ?
+            </span>
+            <button className="header-box-content-cta">
+              Commencer à vendre
+            </button>
+            <span className="header-box-content-about">
+              Découvrir comment ça marche
+            </span>
+          </div>
         </div>
       </div>
     </div>
